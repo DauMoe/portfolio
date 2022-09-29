9ac887844bd83f65ecb873917ed2b8dd565c14c4
Vinh Nguyen <vinhnn@cmcglobal.vn>
2022-09-29 17:25:58 +0700

    Add @type/three package

---
+++ src/components/SceneModels/index.tsx
@@ -0,0 +1,15 @@
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+export type SceneModelsProps = {
+
+}
+
+export const SceneModels = (props: SceneModelsProps) => {
+  // const GLTFModels = useLoader(GLTFLoader, './scene.gltf');
+  return(
+    <>
+
+    </>
+  )
+}
