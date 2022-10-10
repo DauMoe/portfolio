a76a2587267f7e6480576fc3c1aac7d8e88870a7
Le Hoang <lehuyhoang1111999@gmail.com>
2022-10-10 12:44:32 +0700

    Add something

--- components/LittleWitch/index.tsx
+++ components/LittleWitch/index.tsx
@@ -1,19 +1,26 @@
+import { PerspectiveCameraProps } from "@react-three/fiber";
 import { NextPage } from "next";
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as THREE from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const LittleWitch: NextPage = () => {
-    const mainElement = useRef<HTMLDivElement>(null);
+  const mainElement = useRef<HTMLDivElement>(null);
+  const [camera, setCamera] = useState<PerspectiveCameraProps>();
 
   useEffect(():void => {
     const WIDTH = mainElement.current?.getBoundingClientRect().width || window.innerWidth;  
     const HEIGHT = mainElement.current?.getBoundingClientRect().height || window.innerHeight;
     const scene = new THREE.Scene();
+
+    //Set background color
+    // scene.background = new THREE.Color( 0xa0a0a0 );
+    // scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
     
     //Init camera
-    const camera = new THREE.PerspectiveCamera(75, WIDTH/HEIGHT, 1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, WIDTH/HEIGHT, 1, 100);
+    setCamera(camera);
     scene.add(camera);
 
      //Init renderer
@@ -24,7 +31,7 @@ const LittleWitch: NextPage = () => {
      //Init controller
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.autoRotate = true;
-    camera.position.set(-100, 10, 0);
+    camera.position.set(0, 3, 0);
     controls.update();
 
     //Add some light
@@ -37,19 +44,15 @@ const LittleWitch: NextPage = () => {
       renderer.render( scene, camera );
     }	
 
-    const gr = new THREE.Group();
     const loader = new GLTFLoader();
     loader.load('space/scene.gltf',
-      function ( gltf: any ) {
-        gr.add(gltf.scene);
-        const loader1 = new GLTFLoader();
-        loader1.load('little-witch/scene.gltf',
-          function ( gltf: any ) {
-            gr.add(gltf.scene);
-            scene.add(gr);
-            animate();
-          }
-        );
+      function(gltf) {
+        var box = new THREE.Box3().setFromObject(gltf.scene);
+        var center = new THREE.Vector3();
+        box.getCenter(center);
+        gltf.scene.position.sub( center ); // center the model
+        scene.add(gltf.scene);
+        animate();
       },
       function ( xhr ) {
         console.log( xhr );
@@ -60,8 +63,19 @@ const LittleWitch: NextPage = () => {
     );
   }, []);
 
+  const IncreaseCamera = () => {
+    camera?.position.set(0, 10, 0);
+    camera?.updateProjectionMatrix();
+    setCamera(camera);
+  }
+
   return (
-    <div ref={mainElement}></div>
+    <>
+      <div style={{position: "absolute", top: 0, right: 0, width: 100, height: 100, backgroundColor: 'green'}}>
+        <button onClick={IncreaseCamera}>increase</button>
+      </div>
+      <div style={{display: "inline-block"}} ref={mainElement}></div>
+    </>
   );
 }
 
